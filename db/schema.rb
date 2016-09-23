d9d98b22e08c73e417cfe3665ed9859e53ae21bb
Takashi Ohta <t-ohta@mksc.jp>
2016-09-23 19:59:05 +0900

    scaffold book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923095008) do
+ActiveRecord::Schema.define(version: 20160923105400) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
